8dc9a87b6bb5bf7ab56e15011cd661bf87bea09b
APorozco <adrielperez1333@gmail.com>
2021-11-02 15:30:57 -0400

    adelnatnatndo

---
+++ src/main/java/com/crud/Spring/service/ProductoService.java
@@ -0,0 +1,14 @@
+package com.crud.Spring.service;
+
+import java.util.Optional;
+
+import com.crud.Spring.model.Producto;
+
+public interface ProductoService {
+
+	public Producto save(Producto producto);
+	public Optional<Producto> get(Integer id);
+	public void update(Producto producto);
+	public void delete(Integer id);
+	
+}
---
+++ src/main/java/com/crud/Spring/service/ProductoServiceimple.java
@@ -0,0 +1,42 @@
+package com.crud.Spring.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.crud.Spring.model.Producto;
+import com.crud.Spring.repository.IProductoRepository;
+
+
+@Service
+public class ProductoServiceimple implements ProductoService {
+
+	@Autowired
+	private IProductoRepository prod_repo;
+	
+	
+	@Override
+	public Producto save(Producto producto) {
+		// TODO Auto-generated method stub
+		return prod_repo.save(producto);
+	}
+
+	@Override
+	public Optional<Producto> get(Integer id) {
+		
+		return prod_repo.findById(id);
+	}
+
+	@Override
+	public void update(Producto producto) {
+		 prod_repo.save(producto);
+	}
+
+	@Override
+	public void delete(Integer id) {
+		prod_repo.deleteById(id);
+		
+	}
+
+}
