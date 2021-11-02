918a179b576c259f7404fea4a573ffdc2391afd9
APorozco <adrielperez1333@gmail.com>
2021-11-02 13:34:57 -0400

    priner comit

---
+++ src/main/java/com/crud/Spring/CrudSpringComerApplication.java
@@ -0,0 +1,13 @@
+package com.crud.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringComerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringComerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/Spring/CrudSpringComerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringComerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
