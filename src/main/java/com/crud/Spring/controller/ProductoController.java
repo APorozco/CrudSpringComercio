19e490fd357ad936e2e8a6c52eed97cc8b7e543e
APorozco <adrielperez1333@gmail.com>
2021-11-02 17:01:04 -0400

    editando productos

--- src/main/java/com/crud/Spring/controller/ProductoController.java
+++ src/main/java/com/crud/Spring/controller/ProductoController.java
@@ -1,11 +1,13 @@
 package com.crud.Spring.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,10 +18,9 @@ import com.crud.Spring.service.ProductoService;
 @RequestMapping("productos")
 public class ProductoController {
 
-	
 	@Autowired
 	private ProductoService pro;
-	
+
 	@GetMapping("")
 	public String home(Model elModelo) {
 		List<Producto> lista_products = pro.listar();
@@ -32,10 +33,27 @@ public class ProductoController {
 		return "productos/create";
 
 	}
+
 	@PostMapping("/save")
-	public String save(Producto p)
+	public String save(Producto p) {
+		pro.save(p);
+		return "redirect:/productos";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model elModelo) {
+		Producto producto = new Producto();
+		Optional<Producto> op = pro.get(id);
+		producto = op.get();
+		elModelo.addAttribute("producto", producto);
+
+		return "productos/edit";
+	}
+	
+	@PostMapping("/update")
+	public String update(Producto p)
 	{
-		 pro.save(p);
+		pro.update(p);
 		return "redirect:/productos";
 	}
 }
