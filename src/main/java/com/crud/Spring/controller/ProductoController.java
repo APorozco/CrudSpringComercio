094c3eddb796746b13e5d6ecaad32bcccdaa7844
APorozco <adrielperez1333@gmail.com>
2021-11-08 11:37:11 -0500

    implementacion para guardar imagen

--- src/main/java/com/crud/Spring/controller/ProductoController.java
+++ src/main/java/com/crud/Spring/controller/ProductoController.java
@@ -1,5 +1,6 @@
 package com.crud.Spring.controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,20 +11,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.crud.Spring.model.Producto;
 import com.crud.Spring.service.ProductoService;
+import com.crud.Spring.service.UploadFileService;
 
 @Controller
 @RequestMapping("productos")
 public class ProductoController {
 
 	@Autowired
-	private ProductoService pro;
+	private ProductoService ProductoService;
+
+	@Autowired
+	private UploadFileService upload;
 
 	@GetMapping("")
 	public String home(Model elModelo) {
-		List<Producto> lista_products = pro.listar();
+		List<Producto> lista_products = ProductoService.listar();
 		elModelo.addAttribute("productos", lista_products);
 		return "productos/show";
 	}
@@ -35,15 +42,34 @@ public class ProductoController {
 	}
 
 	@PostMapping("/save")
-	public String save(Producto p) {
-		pro.save(p);
+	public String save(Producto p, @RequestParam("img") MultipartFile file) throws IOException {
+
+		if (p.getId() == null) { // cuando se crea prod
+			String nombreImagen = upload.saveImage(file);
+			p.setImagen(nombreImagen);
+		} else {
+			if (file.isEmpty()) {
+				Producto pr = new Producto();
+				pr = ProductoService.get(p.getId()).get();
+				p.setImagen(p.getImagen());
+			} else {
+
+				String nombreImagen = upload.saveImage(file);
+				p.setImagen(nombreImagen);
+
+			}
+
+		}
+
+		ProductoService.save(p);
+
 		return "redirect:/productos";
 	}
 
 	@GetMapping("/edit/{id}")
 	public String edit(@PathVariable Integer id, Model elModelo) {
 		Producto producto = new Producto();
-		Optional<Producto> op = pro.get(id);
+		Optional<Producto> op = ProductoService.get(id);
 		producto = op.get();
 		elModelo.addAttribute("producto", producto);
 
@@ -52,13 +78,13 @@ public class ProductoController {
 
 	@PostMapping("/update")
 	public String update(Producto p) {
-		pro.update(p);
+		ProductoService.update(p);
 		return "redirect:/productos";
 	}
 
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
-		pro.delete(id);
+		ProductoService.delete(id);
 		return "redirect:/productos";
 
 	}
