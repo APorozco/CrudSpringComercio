dccfbdb80c30b5b52db9385a4992d970b4f35c72
APorozco <adrielperez1333@gmail.com>
2021-11-08 11:12:45 -0500

    CLase de servicio para subir fotos

---
+++ src/main/java/com/crud/Spring/service/UploadFileService.java
@@ -0,0 +1,39 @@
+package com.crud.Spring.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.sound.midi.Patch;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadFileService {
+
+	private String folder="images//";
+	
+	public String saveImage(MultipartFile file) throws IOException   
+	{
+		if (!file.isEmpty()) {
+			byte [] bytes = file.getBytes();
+			Path path = Paths.get(folder+file.getOriginalFilename());
+			Files.write(path, bytes);
+			return file.getOriginalFilename();
+		}
+		return "default.jpg";
+	}
+	
+	private void deleteimage(String nombre)
+	{
+		String ruta = "image//";
+		File file= new File(ruta+nombre);
+		file.delete();
+		 
+		
+	}
+	
+}
